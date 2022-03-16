b1d5fadd94d54c0f390986ba7e1572ad841b069c
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-16 13:03:52 +0200

    Update CastomAndroidControls.hx

--- source/android/CastomAndroidControls.hx
+++ source/android/CastomAndroidControls.hx
@@ -68,7 +68,7 @@ class CastomAndroidControls extends MusicBeatState
 
                 var exitbutton = new FlxButton(FlxG.width - 200, 50, "Exit", function()
                 {
-			MusicBeatState.switchState(new options.OptionsState());
+			MusicBeatState.switchState(new OptionsState());
 		});
 		exitbutton.setGraphicSize(Std.int(exitbutton.width) * 3);
                 exitbutton.label.setFormat(null, 16, 0x333333, "center");
@@ -78,7 +78,7 @@ class CastomAndroidControls extends MusicBeatState
 		var savebutton = new FlxButton(exitbutton.x, exitbutton.y + 100, "Save", function()
 		{
 			save();
-			MusicBeatState.switchState(new options.OptionsState());
+			MusicBeatState.switchState(new OptionsState());
 		});
 		savebutton.setGraphicSize(Std.int(savebutton.width) * 3);
                 savebutton.label.setFormat(null, 16, 0x333333, "center");
